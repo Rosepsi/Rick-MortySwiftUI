b01689545497e4354b436064867183a618bb336b
Rosa Herrero <rosaherrero@MacBook-Air-de-Rosa.local>
2024-04-17 20:48:00 +0200

    Create new project

---
+++ Rick&MortySwiftUI/Rick&MortySwiftUI/Rick_MortySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rick_MortySwiftUIApp.swift
+//  Rick&MortySwiftUI
+//
+//  Created by Rosa Herrero on 17/4/24.
+//
+
+import SwiftUI
+
+@main
+struct Rick_MortySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
